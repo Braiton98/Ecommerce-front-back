280b1bfee55b6dfa852881768914654302bb75bc
Brian Caceres <briancaceres98@gmail.com>
2023-12-05 12:39:05 -0300

    login y register css
    Hay mejorar los estilos del login y register en lo posible ponerle clases para que no se pisen por los nombres de otros elementos

--- Ecommerce_Game/src/componentes/Login/LoginPage.jsx
+++ Ecommerce_Game/src/componentes/Login/LoginPage.jsx
@@ -1,34 +1,36 @@
-import { useState} from 'react' 
-import './Login.css' 
+import { useState } from 'react'
+import './Login.css'
 
-function LoginPage () {
-   const [email, setEmail] = useState('')
-   const [password, setPassword] = useState('')
+function LoginPage() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
-   const handleSubmit = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault()
-   }
+    }
 
     return (
-        <div className='login'>
-        <h2>Login</h2>
-        <form className='form_auth' onSubmit={handleSubmit}>
-            <input
-                placeholder='email'
-                type="email" 
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-            />
+        <div className="log-container">
+            <div className='login'>
+                <h2>Login</h2>
+                <form className='form_auth' onSubmit={handleSubmit}>
+                    <input
+                        placeholder='email'
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
 
-            <input 
-                type="password" 
-                placeholder='password' 
-                onChange={(e) => setPassword(e.target.value)} 
-                value={password} 
-            />
-            <button onClick={() => window.location.href="/games"}>Login</button>
-        </form>
-        <p> Do you not have an accout yet?<a href="/register"> Sign up</a></p>
+                    <input
+                        type="password"
+                        placeholder='password'
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                    />
+                    <button onClick={() => window.location.href = "/games"} className=''>Login</button>
+                </form>
+                <p> Do you not have an accout yet?<a href="/register"> Sign up</a></p>
+            </div>
         </div>
     )
 }
--- Ecommerce_Game/src/componentes/Login/Register.jsx
+++ Ecommerce_Game/src/componentes/Login/Register.jsx
@@ -1,54 +1,56 @@
-import { useState } from 'react' ; 
+import { useState } from 'react';
 import './Login.css'
 
 
-function Register()  {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
+function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
-    const handleSubmit = (e) => {
-      e.preventDefault();
-    }
-        
-    return ( 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  return (
+    <div className="reg-container">
       <div className='register'>
         <h1>Create user</h1>
         <h2>Register</h2>
         <form className='form_auth' onSubmit={handleSubmit}>
-            
-            <input 
-              placeholder='name'
-              type="text" 
-              value={name} 
-              onChange={(e) => setName(e.target.value)} 
-            />
-           
-            <input 
-              placeholder='email'
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)} 
-            />
-            
-            <input
-             placeholder='password'
-             type="password" 
-             value={password}
-             onChange={(e) => setPassword(e.target.value)}
-            />
 
-            <input
-              placeholder='confirm password'
-              type="password" 
-              value={confirmPassword} 
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            />
-            <button onClick={() => window.location.href="/games"}>create user</button>
+          <input
+            placeholder='name'
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+
+          <input
+            placeholder='email'
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <input
+            placeholder='password'
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <input
+            placeholder='confirm password'
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <button onClick={() => window.location.href = "/games"}>create user</button>
         </form>
       </div>
-    )
+    </div>
+  )
 }
 
 export default Register
